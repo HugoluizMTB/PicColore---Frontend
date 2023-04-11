027074aa460a51b93d047e2ff859497ecdc1635c
Hugo Luiz <104656207+HugoluizMTB@users.noreply.github.com>
2023-04-11 19:33:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LoginPage from './pages/LoginPage'
+import './styles/App.css'
+
+export const App = () => {
+
+
+  return (
+    {LoginPage}
+  )
+}
+
+export default App
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from '../styles/'
+
+const LoginPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
